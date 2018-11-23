08333594512e23fc2d2ee7972a982c9351b67569
christian <christiancamilo1221@gmail.com>
2018-11-22 21:06:01 -0500

    modulos  h he f cad cont

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  display = 'none';
+  chris = '';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+  onCloseHandled() {
+
+    this.display = 'none';
+
+  }
+
+
+  openModal() {
+
+    this.display = 'block';
+
+  }
+
+
+}
