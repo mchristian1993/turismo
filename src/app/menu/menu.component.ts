6f9b09d2a61bf559026541b33014eb7d871a4e9e
christian <christiancamilo1221@gmail.com>
2018-11-30 20:58:07 -0500

    modificacoin de interfaz centrado de div sitios turisticos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 
 @Component({
@@ -9,6 +9,7 @@ import {Component, OnInit} from '@angular/core';
 export class MenuComponent implements OnInit {
   display = 'none';
   chris = '';
+  tabs = 'none';
 
   constructor() {
 
